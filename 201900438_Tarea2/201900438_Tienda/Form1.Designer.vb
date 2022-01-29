b31c31a6464732ed8b13d680c211d3c7803cfb91
chimbikuzz <98655985+chimbikuzz@users.noreply.github.com>
2022-01-29 10:04:26 -0600

    Tarea 2

---
+++ 201900438_Tarea2/201900438_Tienda/Form1.Designer.vb
@@ -0,0 +1,187 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextArroz = New System.Windows.Forms.TextBox()
+        Me.TextFrijol = New System.Windows.Forms.TextBox()
+        Me.TextAzucar = New System.Windows.Forms.TextBox()
+        Me.TextNombre = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ButtonCalcular = New System.Windows.Forms.Button()
+        Me.ButtonLimpiar = New System.Windows.Forms.Button()
+        Me.ButtonCerrar = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextArroz
+        '
+        Me.TextArroz.BackColor = System.Drawing.Color.Silver
+        Me.TextArroz.Location = New System.Drawing.Point(178, 26)
+        Me.TextArroz.Name = "TextArroz"
+        Me.TextArroz.Size = New System.Drawing.Size(187, 27)
+        Me.TextArroz.TabIndex = 0
+        '
+        'TextFrijol
+        '
+        Me.TextFrijol.BackColor = System.Drawing.Color.Silver
+        Me.TextFrijol.Location = New System.Drawing.Point(178, 77)
+        Me.TextFrijol.Name = "TextFrijol"
+        Me.TextFrijol.Size = New System.Drawing.Size(187, 27)
+        Me.TextFrijol.TabIndex = 1
+        '
+        'TextAzucar
+        '
+        Me.TextAzucar.BackColor = System.Drawing.Color.Silver
+        Me.TextAzucar.Location = New System.Drawing.Point(178, 123)
+        Me.TextAzucar.Name = "TextAzucar"
+        Me.TextAzucar.Size = New System.Drawing.Size(187, 27)
+        Me.TextAzucar.TabIndex = 2
+        '
+        'TextNombre
+        '
+        Me.TextNombre.BackColor = System.Drawing.Color.Silver
+        Me.TextNombre.Location = New System.Drawing.Point(178, 190)
+        Me.TextNombre.Name = "TextNombre"
+        Me.TextNombre.Size = New System.Drawing.Size(287, 27)
+        Me.TextNombre.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(77, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "ARROZ"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(77, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "FRIJOL"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(77, 130)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "AZUCAR"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(29, 197)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(129, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "NOMBRE CLIENTE"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.GroupBox1.Controls.Add(Me.TextArroz)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.TextFrijol)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TextAzucar)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TextNombre)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(108, 48)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(492, 256)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "PRODUCTOS"
+        '
+        'ButtonCalcular
+        '
+        Me.ButtonCalcular.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ButtonCalcular.Location = New System.Drawing.Point(108, 333)
+        Me.ButtonCalcular.Name = "ButtonCalcular"
+        Me.ButtonCalcular.Size = New System.Drawing.Size(94, 29)
+        Me.ButtonCalcular.TabIndex = 9
+        Me.ButtonCalcular.Text = "CALCULAR"
+        Me.ButtonCalcular.UseVisualStyleBackColor = False
+        '
+        'ButtonLimpiar
+        '
+        Me.ButtonLimpiar.BackColor = System.Drawing.Color.DodgerBlue
+        Me.ButtonLimpiar.Location = New System.Drawing.Point(311, 333)
+        Me.ButtonLimpiar.Name = "ButtonLimpiar"
+        Me.ButtonLimpiar.Size = New System.Drawing.Size(94, 29)
+        Me.ButtonLimpiar.TabIndex = 10
+        Me.ButtonLimpiar.Text = "LIMPIAR"
+        Me.ButtonLimpiar.UseVisualStyleBackColor = False
+        '
+        'ButtonCerrar
+        '
+        Me.ButtonCerrar.BackColor = System.Drawing.Color.Red
+        Me.ButtonCerrar.Location = New System.Drawing.Point(506, 333)
+        Me.ButtonCerrar.Name = "ButtonCerrar"
+        Me.ButtonCerrar.Size = New System.Drawing.Size(94, 29)
+        Me.ButtonCerrar.TabIndex = 11
+        Me.ButtonCerrar.Text = "CERRAR"
+        Me.ButtonCerrar.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(1021, 535)
+        Me.Controls.Add(Me.ButtonCerrar)
+        Me.Controls.Add(Me.ButtonLimpiar)
+        Me.Controls.Add(Me.ButtonCalcular)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TextArroz As TextBox
+    Friend WithEvents TextFrijol As TextBox
+    Friend WithEvents TextAzucar As TextBox
+    Friend WithEvents TextNombre As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ButtonCalcular As Button
+    Friend WithEvents ButtonLimpiar As Button
+    Friend WithEvents ButtonCerrar As Button
+End Class
---
+++ 201900438_Tarea2/201900438_Tienda/Form1.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Math
+Public Class Form1
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextFrijol.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextArroz.TextChanged
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub ButtonLimpiar_Click(sender As Object, e As EventArgs) Handles ButtonLimpiar.Click
+        TextArroz.Clear()
+        TextAzucar.Clear()
+        TextFrijol.Clear()
+        TextNombre.Clear()
+
+        TextArroz.Focus()
+
+    End Sub
+
+    Private Sub ButtonCerrar_Click(sender As Object, e As EventArgs) Handles ButtonCerrar.Click
+        End
+
+    End Sub
+
+    Private Sub ButtonCalcular_Click(sender As Object, e As EventArgs) Handles ButtonCalcular.Click
+        'If (TextArroz.Text = 1) Then
+        'MsgBox("Verdadero")
+        'End If
+        Dim Arroz, azucar, frijol, Pago_Normal, Valor_IVA, Pago_Parcial, Descuento, Pago_Total, N, Descuento1 As Double
+        N = 0.12
+        Descuento1 = 0.025
+        Arroz = Val(TextArroz.Text) * 2
+        frijol = Val(TextFrijol.Text) * 1.75
+        azucar = Val(TextAzucar.Text) * 2.5
+
+
+        Pago_Normal = Round(Val(Arroz + frijol + azucar), 2)
+        'MsgBox("Pago Normal: " + Pago_Normal.ToString)
+
+        Valor_IVA = Round(Val(Pago_Normal * N), 2)
+        'MsgBox(Valor_IVA)
+        Pago_Parcial = Round(Val(Pago_Normal + Valor_IVA), 2)
+
+        Descuento = Round(Val(Pago_Parcial * Descuento1), 2)
+        Pago_Total = Round(Val(Pago_Parcial - Descuento), 2)
+        MsgBox(TextNombre.Text + vbNewLine + vbNewLine + "1. Pago Normal: " + Pago_Normal.ToString + vbNewLine + "2. Valor IVA: " + Valor_IVA.ToString + vbNewLine +
+               "3. Pago Parcial: " + Pago_Parcial.ToString + vbNewLine + "4. Descuento: " + Descuento.ToString + vbNewLine + vbNewLine + "           TOTAL: " + Pago_Total.ToString, , "DATOS DE LA VENTA")
+
+
+
+    End Sub
+End Class
